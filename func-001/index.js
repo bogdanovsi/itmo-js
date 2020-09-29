38503c07c388a644ded46d761968ae36bceca66d
bogdanovs <sergey.bogdanov@geropharm.com>
2020-09-29 04:24:23 +0300

    goss_concat

--- func-001/index.js
+++ func-001/index.js
@@ -7,7 +7,7 @@ const rgb = (r = 255, g = 255, b = 255) => {
     g = Math.min(Math.abs(g), 255);
     b = Math.min(Math.abs(b), 255);
 
-    return `#${intToColor(r)}${intToColor(g)}${intToColor(b)}`;
+    return concat('#', intToColor(r), intToColor(g), intToColor(b));
 }
-
+    
 console.log(rgb(0, 34, 123));
